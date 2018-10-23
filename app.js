badb50cf1bb4771a75a3fafc9ceba692881c7806
Marko <markovitiz7@gmail.com>
2018-10-23 18:02:02 +0200

    Added app.js and logger.js

---
+++ app.js
@@ -0,0 +1,56 @@
+console.log(__filename);
+console.log(__dirname);
+/*function sayHello(name) {
+    console.log('Hello ' + name);
+
+}*/
+
+// sayHello('Marko');
+/*
+console.log() //global
+setTimeout()
+clearTimeout()
+
+setInterval()
+clearInterval()
+
+ window.console.log <-> console.log
+
+ // 
+window.message
+global.setTimeout */
+
+
+// console.log(module);
+
+const logger = require('./logger');
+// console.log(logger);
+
+logger('message');
+
+const path = require('path');
+const os = require('os');
+
+var pathObj = path.parse(__filename);
+console.log(pathObj);
+var totmem= os.totalmem();
+var frmem = os.freemem();
+console.log('Total memory: ' + totmem);
+console.log(`Total Memory: ${totmem}\nFree Memory: ${frmem}`);
+
+const fs = require('fs');
+const EventEmitter = require('events') // Veliko slovo -> Klasa
+const files = fs.readdirSync('./');
+fs.readdir('./', function(err, files) {
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
+});
+console.log(files);
+const emitter = new EventEmitter();
+//Listener
+emitter.on('messageLogged', function(eventArg){
+    console.log('Listener called', eventArg.url);
+});
+// Async > Sync
+
+emitter.emit('messageLogged', {id:1, url: 'https://www.google.rs/'}); // emit - singaling that an event has happened
