1b06c8c549399b5479a2f3ed94786fb3ab14f3f3
Marko <markovitiz7@gmail.com>
2018-10-23 18:23:18 +0200

    Advanced further in the tutorial section

--- app.js
+++ app.js
@@ -54,3 +54,23 @@ emitter.on('messageLogged', function(eventArg){
 // Async > Sync
 
 emitter.emit('messageLogged', {id:1, url: 'https://www.google.rs/'}); // emit - singaling that an event has happened
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        res.write('Hello World');
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+});
+server.on('connection', (socket) => {
+    console.log('New connection...');
+})
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
